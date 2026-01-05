f1944cf5d215b8d9b3fbfc2687f1ba93e079150f
Jank8 <99371372+Jank8@users.noreply.github.com>
2026-01-05 08:36:57 +0100

    Version

--- FlairX-Mod-Manager/AppConstants.cs
+++ FlairX-Mod-Manager/AppConstants.cs
@@ -3,7 +3,7 @@ namespace FlairX_Mod_Manager
     public static class AppConstants
     {
         // Application Version
-        public const string APP_VERSION = "3.4.8";
+        public const string APP_VERSION = "3.4.9";
         
         // File Extensions
         public const string MOD_JSON_FILENAME = "mod.json";
