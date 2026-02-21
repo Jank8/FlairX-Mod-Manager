ae2314618a77f0f769db79d275bb6536ab5ff383
Jank8 <wozniak.janusz@protonmail.com>
2026-02-21 18:24:16 +0100

    Version

--- FlairX-Mod-Manager/AppConstants.cs
+++ FlairX-Mod-Manager/AppConstants.cs
@@ -3,7 +3,7 @@ namespace FlairX_Mod_Manager
     public static class AppConstants
     {
         // Application Version
-        public const string APP_VERSION = "3.8.1";
+        public const string APP_VERSION = "3.8.3";
         
         // File Extensions
         public const string MOD_JSON_FILENAME = "mod.json";
