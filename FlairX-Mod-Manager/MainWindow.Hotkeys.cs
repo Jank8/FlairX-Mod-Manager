c8234e63ebbcfb4bcb88d48e1f909bd544e22f73
Jank8 <99371372+Jank8@users.noreply.github.com>
2026-08-06 11:57:25 +0200

    Fix

--- FlairX-Mod-Manager/MainWindow.Hotkeys.cs
+++ FlairX-Mod-Manager/MainWindow.Hotkeys.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
