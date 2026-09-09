f3018d73378595955d9fd58a84e77364ccec15a1
Jank8 <99371372+Jank8@users.noreply.github.com>
2026-09-09 12:10:38 +0200

    Bump version to 4.1.4

--- FlairX-Mod-Manager/AppConstants.cs
+++ FlairX-Mod-Manager/AppConstants.cs
@@ -3,8 +3,8 @@ namespace FlairX_Mod_Manager
     public static class AppConstants
     {
         // Application Version
-        public const string APP_VERSION = "4.1.3";
-        public const string APP_VERSION_DISPLAY = "4.1.3";
+        public const string APP_VERSION = "4.1.4";
+        public const string APP_VERSION_DISPLAY = "4.1.4";
         
         // File Extensions
         public const string MOD_JSON_FILENAME = "mod.json";
